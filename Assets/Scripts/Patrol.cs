0dcdea8aa44d335217155676e21f872ec89fb505
GeordieGreen <68436694+GeordieGreen@users.noreply.github.com>
2020-08-14 09:24:34 -0700

    Initial commit

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed = 20f;
+    public float waitTime = 1f;
+    private float currentWait = 0f;
+
+    float minX;
+    float maxX;
+    float minZ;
+    float maxZ;
+    Vector3 moveSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GroundSize();
+        moveSpot = GetNewPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Rotation();
+    }
+
+    void GroundSize()
+    {
+        GameObject ground = GameObject.FindWithTag("Ground");
+        Renderer gScale = ground.GetComponent<Renderer>();
+        minX = (gScale.bounds.center.x - gScale.bounds.extents.x);
+        maxX = (gScale.bounds.center.x + gScale.bounds.extents.x);
+        minZ = (gScale.bounds.center.z - gScale.bounds.extents.z);
+        maxZ = (gScale.bounds.center.z + gScale.bounds.extents.z);
+    }
+    Vector3 GetNewPos()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomZ = Random.Range(minZ, maxZ);
+        Vector3 newPos = new Vector3(randomX, transform.position.y, randomZ);
+        return newPos;
+    }
+
+    void Movement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpot, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveSpot) <= 0.2f)
+        {
+            if (currentWait <= 0)
+            {
+                moveSpot = GetNewPos();
+                currentWait = waitTime;
+            }
+            else
+            {
+                currentWait = Time.deltaTime;
+            }
+        }
+    }
+
+    void Rotation()
+    {
+        Vector3 targetDir = moveSpot - transform.position;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, 0.5f, 0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private CharacterController controller;
+
+    [SerializeField] int playerHitPoints = 100;
+    [SerializeField] int playerDamage = 10;
+    [SerializeField] float speed = 5.0f;
+    public float gravity = 9.81f;
+    public float jumpForce = 30f;
+    public float doubleJump = 0.5f;
+
+    public Animator anim;
+
+    float coinCounter;
+    float directionY;
+
+    private bool isDoubleJump = false;
+
+    [SerializeField]
+    Transform cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        
+        float yMovement = Input.GetAxisRaw("Vertical");
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalking",true);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        Vector3 rightWorldMovement = cam.transform.right;
+        Vector3 forwardWorldMovement = cam.transform.forward;
+        forwardWorldMovement.y = 0;
+        forwardWorldMovement.Normalize();
+        
+
+        Vector3 worldMoveInput = Vector3.ClampMagnitude(rightWorldMovement * xMovement + forwardWorldMovement * yMovement, 1);
+        if (worldMoveInput != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(worldMoveInput);
+        }
+
+        if (controller.isGrounded)
+        {
+            isDoubleJump = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                directionY = jumpForce;
+                
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && isDoubleJump)
+            {
+                directionY = jumpForce * doubleJump;
+                isDoubleJump = false;
+                anim.SetBool("isJumping", true);
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("isJumping", true);
+        }
+
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+        
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 20f;
+            anim.SetBool("isSprinting", true);
+            //anim.SetBool("isWalking", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 10f;
+            anim.SetBool("isSprinting", false);
+            anim.SetBool("isWalking", true);
+        }
+
+        
+
+        directionY -= gravity * Time.deltaTime;
+
+        worldMoveInput.y = directionY;
+
+        controller.Move(worldMoveInput * speed * Time.deltaTime);
+
+        if (playerHitPoints <= 0)
+        {
+            SceneManager.LoadScene("DeathScreen");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            coinCounter++;
+            Debug.Log(coinCounter);
+        }
+        if (other.gameObject.CompareTag("EndTrigger"))
+        {
+            if (coinCounter == 3)
+            {
+               SceneManager.LoadScene("EndGame");
+            }
+            else
+            {
+                print("Get All Coins");
+            }
+        }
+        if (other.gameObject.tag == "Enemy")
+        {
+            playerHitPoints -= playerDamage;
+            print(playerHitPoints);
+        }
+    }
+}
